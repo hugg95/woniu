6d78b0ebcce8786757d4a87eece064ac2628455e
linianchao <linianchao@meituan.com>
2015-09-27 22:51:59 +0800

    refined package

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ import tornado.web
 import tornado.template
 import os.path
 import settings
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
+import routers.mainHandler
 
 if __name__ == '__main__':
-    print settings.settings
     application = tornado.web.Application([
-        (r'/', MainHandler),
+        (r'/', routers.mainHandler.MainHandler),
     ], **settings.settings)
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
