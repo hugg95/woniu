634aaf3d300e101f5f70666a8160d9ca1f5bef62
linianchao <linianchao@meituan.com>
2015-10-07 17:53:31 +0800

    upd: update style

--- routers/mainHandler.py
+++ routers/mainHandler.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import tornado.web
 import modules.db
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        size = 50
+    def get(self, pageSize=50, pageNum=1):
         posts = modules.db.db.query('select * from post')
+        for post in posts:
+            post.title_display = post.type and u'[求租]' + post.title or u'[出租]' + post.title
+
         self.render('index.html', posts=posts)
--- routers/userHandler.py
+++ routers/userHandler.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import tornado.web
 
 class LoginHandler(tornado.web.RequestHandler):
