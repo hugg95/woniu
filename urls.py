ab679a564438788dba36cfeff6333293242a4020
linianchao <linianchao@meituan.com>
2015-10-07 17:05:03 +0800

    add: add post page

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 import routers.mainHandler as main
+import routers.postHandler as post
 import routers.userHandler as user
 
 urls = [
     (r'/', main.MainHandler),
     (r'/user/login', user.LoginHandler),
+    (r'/post/(\d+)', post.PostHandler),
 ]
