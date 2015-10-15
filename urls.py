180a5365350f258a5f27a679c506882dbc0250e5
lncwwn <lncwwn@gmail.com>
2015-10-16 00:26:41 +0800

    add: add get comments list api

--- urls.py
+++ urls.py
@@ -1,12 +1,14 @@
 import routers.baseHandler as base
 import routers.mainHandler as main
 import routers.postHandler as post
+import routers.commentHandler as comment
 import routers.userHandler as user
 
 urls = [
     (r'/', main.MainHandler),
     (r'/posts/last', post.ListHandler),
     (r'/posts/(\d+)', post.PostHandler),
+    (r'/comments/list', comment.ListHandler),
     (r'/users/login', user.LoginHandler),
     (r'/users/signup', user.SignupHandler),
     (r'/users/(\d+)', user.ProfileHandler),
