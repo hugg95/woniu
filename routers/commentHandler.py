a919cf78d177de84d2e92b609a4e585ec1d0e3eb
lncwwn <lncwwn@gmail.com>
2015-10-16 10:21:25 +0800

    upd: update commentHandler

---
+++ routers/commentHandler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author victor li nianchaoli@msn.cn
+# @date 2015/10/15
+
+import baseHandler
+import modules.db
+
+class ListHandler(baseHandler.RequestHandler):
+
+    def get(self):
+        post_id = 1
+        if not post_id:
+            self.write(None)
+            self.finish()
+            return
+
+        query = 'select id, content, member_id, post_id, created, updated from comment where post_id = %s'
+        _comments = modules.db.db.query(query, post_id)
+        comments = {'comments': _comments}
+
+        self.write(comments)
+        self.finish()
+
